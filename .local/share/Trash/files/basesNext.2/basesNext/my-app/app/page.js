d6feb0a8d750477d097e1fc97ff49da63a863a08
hpierrac <h.pierrache@gmail.com>
2025-04-22 16:13:00 +0200

    create-project

---
+++ .local/share/Trash/files/basesNext.2/basesNext/my-app/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Bienvenue sur mon premier projet Next.js!</h1>
+      <p>Ceci est une page basique créée avec Next.js</p>
+    </main>
+  )
+}
